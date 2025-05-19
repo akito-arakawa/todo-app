6f040c43aee237eea865e7449cb97bd3ac3ba642
akito <kd1373996@st.kobedenshi.ac.jp>
2025-05-19 12:45:51 +0900

    config修正

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
+  output: "standalone",
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 };
+
+export default nextConfig;
